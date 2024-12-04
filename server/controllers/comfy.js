519baf9bf990dfe5f417bd332dbfe1e9c30f50bd
Hiep Nguyen <nguyendunghiep20012003@gmail.com>
2024-12-04 17:37:52 +0700

    test

--- server/controllers/comfy.js
+++ server/controllers/comfy.js
@@ -22,7 +22,7 @@ const loras = {
   japanese: {
     lora_name: "japanese_interior_lora.safetensors",
     trigger_words: "Japanese Interior",
-    prompt_style: "a japanese style interior room design",
+    prompt_style: "minimal, serene, natural materials",
     clip_skip: 1,
   },
   luxury: {
