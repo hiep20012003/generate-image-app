278b12962e2fcbd26f7170ea066a10402e73a470
Hiep Nguyen <nguyendunghiep20012003@gmail.com>
2024-12-04 19:06:09 +0700

    test

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,12 +1,9 @@
 import Home from '../pages/Home.js';
 import Text2Image from '../pages/Text2Image.js';
-import Sketch2Image from '../pages/Sketch2Image.js';
 
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/text2image', component: Text2Image },
-    { path: '/Sketch2Image', component: Sketch2Image },
-    { path: '/changeStyle', component: ChangeStyle },
 ]
 
 export { publicRoutes };
